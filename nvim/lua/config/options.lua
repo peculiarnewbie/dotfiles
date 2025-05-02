ca41f8c26be437f1e9fdb71e9c991c426b7519cd
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2025-05-02 22:11:51 +0700

    disable nvim animation

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- disable animations
+vim.g.snacks_animate = false
+
+-- prettier only when config
 vim.g.lazyvim_prettier_needs_config = true
+
+-- tab as 4
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
