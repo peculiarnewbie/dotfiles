82e8cd5c4d37abd6fbca82bd67e837c38a9352f0
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2025-07-11 14:20:07 +0700

    simplify komo

--- scripts/komo-alternate.ps1
+++ scripts/komo-alternate.ps1
@@ -1,12 +1,5 @@
-$i = 0
 while ($true) {
-    if ($i % 2 -eq 0) {
-        Write-Host "Running: komorebic cycle-workspace next"
-        komorebic cycle-workspace next
-    } else {
-        Write-Host "Running: komorebic cycle-workspace previous"
-        komorebic cycle-workspace previous
-    }
-    $i++
+    Write-Host "Running: komorebic cycle-workspace next"
+    komorebic cycle-workspace next
     Start-Sleep -Seconds 180
 }
