6cd5858d714291e4e312f4399b3fad7b57b0fe79
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2025-10-17 14:58:06 +0700

    komo random

--- scripts/komo-alternate.ps1
+++ scripts/komo-alternate.ps1
@@ -1,5 +1,6 @@
 while ($true) {
     Write-Host "Running: komorebic cycle-workspace next"
-    komorebic cycle-workspace next
-    Start-Sleep -Seconds 180
+    & komorebic cycle-workspace next
+    $val = Get-Random -Minimum 30 -Maximum 200
+    Start-Sleep -Seconds $val
 }
